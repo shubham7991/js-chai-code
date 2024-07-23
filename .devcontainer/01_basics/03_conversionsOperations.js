bfa4115c7fa356d5c8baaae644cb6b70fcc1dd73
shubham7991 <129823331+shubham7991@users.noreply.github.com>
2024-07-23 14:30:58 +0000

    conversionsofDataTypes

---
+++ .devcontainer/01_basics/03_conversionsOperations.js
@@ -0,0 +1,18 @@
+let score="33"
+//console.log(typeof score)
+//console.log(typeof(score))
+let valueInNumber = Number(score)
+//console.log(typeof (valueInNumber));
+
+//true=1 & false=0
+//"33"=33
+//"33abc"=NaN
+let isloggedIn =""
+let booleanIsloggedIn = Boolean(isloggedIn)
+//console.log(booleanIsloggedIn)
+//""=false 
+//"shubham"= true
+let someNumber =45
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+
