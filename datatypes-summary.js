8b265381ea53924495ba675107d2d6947391fd9f
shubham7991 <129823331+shubham7991@users.noreply.github.com>
2024-07-24 06:21:38 +0000

    datatypes

---
+++ datatypes-summary.js
@@ -0,0 +1,36 @@
+// PRIMITIVE
+
+//7TYPES: string, number, boolean, null, undefined, symbol , big int
+
+const score =100
+const scoreValue= 100.3
+
+const isLoggedIn = false
+const temp = null
+
+let userEmil;   //undefined
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id=== anotherId);
+//const bigNumber = 345556445233324889n
+
+
+
+// REFERENCE TYPE(NON PRIMITIVE)
+
+//arrays , objects, functions
+
+const heroes =["shaktiman","naagraj","doga"]
+let myObj ={
+    name: "shubham",
+    age : 21,
+}
+
+const myfunction = function(){
+    console.log("hello world");
+}
+console.log(typeof temp)
+console.log(typeof scoreValue);
+console.log(typeof myfunction);
