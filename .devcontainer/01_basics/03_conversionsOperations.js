fa9feae9b6ce655ab0ae4b99193d67ff111d7d4d
shubham7991 <129823331+shubham7991@users.noreply.github.com>
2024-07-24 05:34:27 +0000

    comparisons.js

--- .devcontainer/01_basics/03_conversionsOperations.js
+++ .devcontainer/01_basics/03_conversionsOperations.js
@@ -14,5 +14,33 @@ let booleanIsloggedIn = Boolean(isloggedIn)
 //"shubham"= true
 let someNumber =45
 let stringNumber = String(someNumber)
-console.log(stringNumber)
+//console.log(stringNumber)
+//*************************** OPERATIONS */
 
+let value = 3
+let negVlaue = -value
+//console.log(negVlaue); 
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/2)
+// console.log(2%3)
+
+let str1 = "ramram"
+let str2 = "shubh"
+let str3 = str1 + str2
+// console.log(str3)
+// console.log(1+"2")
+// console.log("1"+2)
+// console.log("1"+2+2)
+// console.log(1+2+"2")
+
+// console.log(+true)
+// console.log(+"")
+// console.log(+false)
+
+let gamecounter=220
+//++gamecounter;
+gamecounter++;
+console.log(gamecounter)
