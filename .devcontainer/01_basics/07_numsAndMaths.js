28190be4c796efb81a23fa71fbf0a99cce3f72e5
shubham7991 <129823331+shubham7991@users.noreply.github.com>
2024-08-25 16:48:03 +0000

    math and num

---
+++ .devcontainer/01_basics/07_numsAndMaths.js
@@ -0,0 +1,32 @@
+const score = 5000
+ //console.log(score)
+
+ const balance = new Number(300)
+ //console.log(balance)
+//console.log(balance.toString().length)
+//console.log(typeof(Number));
+//console.log(balance.toFixed(3))
+
+const otherNumber = 123.4642
+//console.log(otherNumber.toPrecision(3));
+
+const thousands = 1000000000
+//console.log(thousands.toLocaleString('en-In'));
+
+//*****************MATHS*************
+
+console.log(Math);
+console.log(Math.abs(-3));
+console.log(Math.round(3.45));
+console.log(Math.ceil(3.1));
+console.log(Math.floor(3.9));
+
+const min =10;
+const max =20;
+
+console.log(Math.floor(Math.random()*(max- min+1)+min));
+
+
+
+
+
