ea742d3c0412b7e5d52bce839cd5e111ff4c2c9e
shubham7991 <129823331+shubham7991@users.noreply.github.com>
2024-08-25 13:27:42 +0000

    strings

---
+++ .devcontainer/01_basics/strings.js
@@ -0,0 +1,27 @@
+const name = "shubham"
+const repocount = 5
+
+//console.log(name+repocount+"value");
+//console.log(`hello my name is${name} and my repo count is${repocount}`);
+const gameName= new String('shubhhp')
+//console.log(gameName[7]);
+//console.log(gameName.__proto__)
+
+// console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt('5'));
+const newString = gameName.substring(0,4)
+console.log(newString);
+const anotherString = gameName.slice(-10,4)
+console.log(anotherString)
+
+const newStringOne ="      .shubh.     "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url ="https://web.whatsapp.com/"
+
+console.log(url.replace('web', 'ppo'))
+console.log(url.includes('ppo'))
+
+console.log(gameName.split('////'))
