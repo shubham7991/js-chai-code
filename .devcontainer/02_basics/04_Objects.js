57e792d0979927ab11fe712226eb17cd39e36b3c
shubham7991 <129823331+shubham7991@users.noreply.github.com>
2024-08-28 08:40:05 +0000

    objectss

---
+++ .devcontainer/02_basics/04_Objects.js
@@ -0,0 +1,36 @@
+//const tinderUser = new Object()
+const tinderUser = {}
+//console.log(tinderUser);
+
+tinderUser.id = "123sam"
+tinderUser.name = "sam"
+tinderUser.isLoggedIn = false
+
+//console.log(tinderUser);
+
+const regularUSer = {
+    email : "shubh@gmail.com",
+    fullName:{
+        userfullName: {
+            firstName: "shubh"
+        }
+    }
+}
+//console.log(regularUSer.email);
+const obj1 = {1:"a",2:"b"}
+const obj2 = {3:"a", 4: "b"}
+
+//const obj3 = {obj1 , obj2}
+//const obj3 = Object.assign({},obj1 , obj2)
+const obj3 = {...obj1, ...obj2}
+//console.log(obj3);
+
+const course = {
+    courseName : "jsHindi",
+    price : "999",
+    courseInstructor : "hitesh"
+
+}
+//course.courseInstructor
+const {courseInstructor} = course
+console.log(courseInstructor);
