bf1890173f54b2d03f1488c1aea3889c3c0fad1f
shubham7991 <129823331+shubham7991@users.noreply.github.com>
2024-08-30 13:02:54 +0000

    one

---
+++ .devcontainer/04_control_flow/one.js
@@ -0,0 +1,52 @@
+//if
+// const temperature = 50
+// const isUserLoggedIn = true
+// if(temperature<50){
+//     console.log("less than 50");
+    
+
+// } else {
+// console.log("temp is greater than 50");
+// }
+// const score =200
+// if (score>100){
+//     var power = "fly"    //if we use var incase of const then error will not occur and var is of global scope
+//     console.log(`user power: ${power}`);
+    
+// }
+// console.log(`user power: ${power}`);
+// console.log(`user power: ${power}`);
+
+const balance =340
+
+//if(balance>500) console.log("test") ;
+
+// if (balance<500){
+//     console.log("less than 500");
+    
+// } else if(balance<750){
+//     console.log("less than 750");
+    
+// } else if (balance<900){
+//     console.log("less than 900");
+    
+// } else{
+//     console.log("less than 1200");}
+
+
+const UserLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromemail = true
+
+if (UserLoggedIn && debitCard ){
+    console.log("allow to buy course");
+    
+}
+if (loggedInFromGoogle || loggedInFromemail){
+    console.log("user logged in");
+    
+
+}
+
+
